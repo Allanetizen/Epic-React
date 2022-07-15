612b8992fca79554cee9891c74284c89ae699df4
Allanetizen <allankosgei747@gmail.com>
2022-07-15 08:25:51 +0300

    className prop in styling

--- src/exercise/05.js
+++ src/exercise/05.js
@@ -14,9 +14,9 @@ import '../box-styles.css'
 // 🐨 also use the style prop to make the font italic
 // 💰 Here are available style attributes: backgroundColor, fontStyle
 
-const smallBox = <div>small lightblue box</div>
-const mediumBox = <div>medium pink box</div>
-const largeBox = <div>large orange box</div>
+const smallBox = <div className='box box--small'>small lightblue box</div>
+const mediumBox = <div className='box box--medium'>medium pink box</div>
+const largeBox = <div className='box box--large'>large orange box</div>
 
 function App() {
   return (
